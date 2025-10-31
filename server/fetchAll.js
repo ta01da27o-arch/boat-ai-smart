de145eda4974f1347be4d2daada5394e0dc8db9c
ta01da27o-arch <ta01da27o@gmail.com>
2025-10-31 22:01:30 +0900

    Delete server/fetchAll.js

--- server/fetchAll.js
+++
@@ -1,8 +0,0 @@
-// server/fetchAll.js
-import { scrapeAllVenues } from "./scrape.js";
-
-(async () => {
-  console.log("🚀 出走表自動取得開始...");
-  await scrapeAllVenues();
-  console.log("✅ 全場データ更新完了。");
-})();
